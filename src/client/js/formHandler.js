0631f32627129c0ffbfafe95d781aeefc488a905
motasemodeh <motasem.udeh@gmail.com>
2023-09-02 18:32:16 +0300

    fetching data from weatherbit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,36 +1,49 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-
-    // check what text was put into the form field
+    // Check what text was put into the form field
     const cityName = document.getElementById('destination').value;
 
+    // Updating the UI with data received from the server
+    if (cityName) {
+        console.log(`Form Submitted ${cityName}`);
+        document.querySelector('.results').style.display = 'block';
+        document.querySelector('#error').style.display = 'none';
 
-    // updating the UI with data received from the server
-    if(cityName) {
-    console.log(`Form Submitted ${cityName}`)
-    document.querySelector('.results').style.display = 'block';
-    document.querySelector('#error').style.display = 'none';
-    postData('http://localhost:9020/api', {url: cityName})
+        // Send a POST request to fetch geonames data
+        postData('/api', { url: cityName })
+            .then(function (geonamesRes) {
+                // Extract latitude (lat) and longitude (lng) from the Geonames response
+                const lat = geonamesRes.geonames[0].lat;
+                const lng = geonamesRes.geonames[0].lng;
 
-    .then(function(res) {
+                // Send a POST request to fetch weather data based on lat and lng
+                postData('/weatherbit', { lat, lng })
+                    .then(function (weatherbitRes) {
+                        // Update the HTML elements with weather data
+                        document.getElementById('weather-description').textContent = `Weather: ${weatherbitRes.data[0].weather.description}`;
+                        document.getElementById('temperature').textContent = `Temperature: ${weatherbitRes.data[0].temp}°C`;
+                        // Add more elements for other weather data you want to display
 
-        document.getElementById('lan').innerHTML = `country: ${res.geonames[0].countryName}`;
-        document.getElementById("lat").innerHTML = `lan:: ${res.geonames[0].lat}`;
-        document.getElementById("country").innerHTML = `lant: ${res.geonames[0].lng}`;
-        
-    })
+                        // Update other elements with weather data as needed
+                    })
+                    .catch(function (weatherbitError) {
+                        console.error('Error fetching Weatherbit data:', weatherbitError);
+                    });
+            })
+            .catch(function (geonamesError) {
+                console.error('Error fetching Geonames data:', geonamesError);
+            });
     } else {
         document.querySelector('.results').style.display = 'none';
         document.querySelector('#error').style.display = 'block';
-        document.getElementById('error').innerHTML = 'Appears to be an incorrect URL. Please attempt with a valid URL.';
-        console.log(cityName)
-        
+        document.getElementById('error').innerHTML =
+            'Appears to be an incorrect URL. Please attempt with a valid URL.';
+        console.log(cityName);
     }
 }
 
-
-const postData = async (url = "", data = {}) => {
+const postData = async (url = '', data = {}) => {
     console.log('Analyzing:', data);
 
     // Send a POST request using fetch
@@ -39,35 +52,21 @@ const postData = async (url = "", data = {}) => {
         credentials: 'same-origin',
         mode: 'cors',
         headers: {
-        'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
     });
     try {
         const newData = await response.json();
-        console.log('Data received:', newData)
+        console.log('Data received:', newData);
         // Return the parsed data
         return newData;
     } catch (error) {
-        console.error('Error in postData:!!!', error);
+        console.error('Error in postData:', error);
 
         // Return an error object with a message
         return { error: true, message: 'An error occurred during the request.' };
     }
 };
-
-// API response output 
-// const polarityChecker = (score) => {
-//     const sentimentMap = {
-//         'P+': 'strong positive',
-//         'P': 'positive',
-//         'NEW': 'neutral',
-//         'N': 'negative',
-//         'N+': 'strong negative',
-//         'NONE': 'no sentiment',
-//     };
-
-//     return sentimentMap[score] ? sentimentMap[score].toUpperCase() : 'Unknown';
-// };
 
 export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -8,10 +8,16 @@ const fetch = require('isomorphic-fetch');
 
 const app = express();
 
-// API variables
+// API Geonames variables
 const baseURL = 'http://api.geonames.org/searchJSON?q=';
 const geonamesParameters = '&maxRows=10&fuzzy=0.8&';
 const apiKey = process.env.GEO_USERNAME;
+
+//Weatherbit Api Variables
+
+const weatherbitApiKey = process.env.WEATHERBIT_KEY;
+
+const API_KEY = process.env.PIXABAY_API_KEY;
 
 
 app.use(cors());
@@ -41,6 +47,32 @@ app.post('/api', async function (req, res, next) {
     }
 });
 
+
+app.post('/weatherbit', async function handleWeatherForecastRequest(req, res) {
+    const lat = req.body.lat;
+    const lng = req.body.lng;
+
+    if (lat && lng) {
+        const WeatherURL = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lng}&key=${weatherbitApiKey}`;
+        
+        try {
+            const response = await fetch(WeatherURL);
+            if (response.ok) {
+                const jsonRes = await response.json();
+                res.status(200).json(jsonRes);
+            } else {
+                res.status(response.status).json({ error: 'Weatherbit request failed' });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    } else {
+        res.status(400).json({ error: 'Bad Request. Missing latitude (lat) and/or longitude (lng).' });
+    }
+});
+
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
@@ -53,3 +85,4 @@ const port = 9020;
 app.listen(port, function () {
     console.log(`app listening on port ${port}`);
 });
+
